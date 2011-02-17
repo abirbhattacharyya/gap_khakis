3ffe08934db5357e5936d357845ee4a0073d515e
dhavalp <dhaval.parikh33@gmail.com>
2011-02-17 15:04:48 +0530

    migration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,17 @@
 class HomeController < ApplicationController
+  before_filter :check_login, :only => [:notifications]
+
   def index
     flash.discard
     if logged_in?
       if current_user.profile.nil?
         render :template => "users/profile"
       elsif current_user.products.size <= 0
-        render :template => "users/products"
+        render :template => "products/products"
       else
         @notifications = [["profile", current_user.profile.created_at], ["prices", current_user.products.last.created_at]]
         @notifications.sort!{|n1, n2| n2[1] <=> n1[1]}
         render :template => "/home/notifications"
-        #@products = current_user.products
       end
 #    else
 #      render :template => "users/biz"
@@ -41,6 +42,36 @@ class HomeController < ApplicationController
 
   def winners
     @payments = Payment.all(:order => "id desc", :limit => 100)
+  end
+
+  def get_twitter_data
+    @id = (TwitterResult.count > 0) ? TwitterResult.last.id : 1
+#    url = "http://172.18.100.96:3005/home/twitter_datas?id=#{@id}"
+    url = "http://174.143.243.249:3000//home/twitter_datas?id=#{@id}"
+    @xml_data = open(url).read
+    @data = Hpricot::XML(@xml_data)
+    @total = 0
+    (@data/"twitter-restaurant").each do |twitter_restaurant|
+      @twitter_result = TwitterResult.new
+      @twitter_result.screen_name = (twitter_restaurant.at('screen-name').innerHTML).strip if twitter_restaurant.at('screen-name')
+      @twitter_result.tweet_id = (twitter_restaurant.at('tweet-id').innerHTML).strip if twitter_restaurant.at('tweet-id')
+      @twitter_result.tweet = (twitter_restaurant.at('tweet').innerHTML).strip if twitter_restaurant.at('tweet')
+      @twitter_result.source = CGI.unescapeHTML(twitter_restaurant.at('source').innerHTML).strip if twitter_restaurant.at('source')
+      @twitter_result.geo = (twitter_restaurant.at('geo').innerHTML).strip if twitter_restaurant.at('geo')
+      @twitter_result.tweet_length = (twitter_restaurant.at('tweet-length').innerHTML).strip if twitter_restaurant.at('tweet-length')
+      @twitter_result.has_link = (twitter_restaurant.at('has-link').innerHTML).strip if twitter_restaurant.at('has-link')
+      @twitter_result.positive = (twitter_restaurant.at('positive').innerHTML).strip if twitter_restaurant.at('positive')
+      @twitter_result.negative = (twitter_restaurant.at('negative').innerHTML).strip if twitter_restaurant.at('negative')
+      @twitter_result.spam = (twitter_restaurant.at('spam').innerHTML).strip if twitter_restaurant.at('spam')
+      @twitter_result.has_question = (twitter_restaurant.at('has-question').innerHTML).strip if twitter_restaurant.at('has-question')
+      @twitter_result.tweet_created = (twitter_restaurant.at('tweet-created').innerHTML).strip if twitter_restaurant.at('tweet-created')
+      @twitter_result.keyword = (twitter_restaurant.at('keyword').innerHTML).strip if twitter_restaurant.at('keyword')
+      if @twitter_result.save
+        @total += 1
+      end
+    end
+    flash[:notice] = "#{@total} new records added"
+    redirect_to root_path
   end
 
 	def code_generate
