ed37debd7d3619cb15af0de401689b8150960323
dhavalp <dhaval.parikh33@gmail.com>
2011-03-09 13:13:27 +0530

    changes

--- config/environment.rb
+++ config/environment.rb
@@ -43,11 +43,10 @@ ActionMailer::Base.default_content_type = "text/html"
 ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :enable_starttls_auto => true,
-  :address => "smtp.gmail.com",
+#ActionMailer::Base.smtp_settings = {
+  :address => "smtp.sendgrid.net",
   :port => 587,
   :authentication => :plain,
-  :user_name => "testing.dealkat@gmail.com",
-  :password => "princessthecat"
+  :user_name => "abstartup@gmail.com",
+  :password => "kdtaxQ28zs6"
 }
